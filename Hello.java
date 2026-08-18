dd30f22401702f202f7b68233cdeda9bc59e9586
Linda <jeropkoimur@gmail.com>
2026-08-18 12:04:47 +0300

    Alternative

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello World!"); // without ln after print, the work looks disorganised. try running the coe this way.
+    }
+}
