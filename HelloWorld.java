5dfd1426e48b36014d2765d02b8b85d7de83cf77
Linda <jeropkoimur@gmail.com>
2026-08-18 11:33:11 +0300

    Java Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// line 2 defines a blueprint for your program. In Java, the class name must exactly match the file name (HelloWorld)
+public class HelloWorld {  
+    // public static void main: The starting point (entry method) where Java begins executing your program.
+    public static void main(String[] args) {
+        // System.out.println: The built-in command that prints text to your screen or terminal window.
+        System.out.println("Hello, World!");    
+    }
+}
