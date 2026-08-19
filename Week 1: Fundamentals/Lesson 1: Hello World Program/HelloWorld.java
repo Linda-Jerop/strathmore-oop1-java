ab657be181fdcac061341dd8159fa55226c07b53
Linda <jeropkoimur@gmail.com>
2026-08-19 13:42:23 +0300

    Unpushed Lesson 1 work

---
+++ Week 1: Fundamentals/Lesson 1: Hello World Program/HelloWorld.java
@@ -0,0 +1,17 @@
+// line 2 defines a blueprint for your program. In Java, the class name must exactly match the file name (HelloWorld)
+public class HelloWorld {  
+    // public static void main: The starting point (entry method) where Java begins executing your program.
+    public static void main(String [] args) {
+        // System.out.println: The built-in command that prints text to your screen or terminal window.
+        System.out.println("Hello, World!");    
+    }
+}
+
+// class methods are designated as static
+// main methods (public) it's access is public. It can be public, private or protected.
+// To print in java, we use a print function: print... and if you want the sursor to go to the next line after printing, you add ln... 
+// so it will be println("Hello World")
+// This print function is stored in a command called System.out
+// Everything in Java must be defined in a class
+// Functions must be encapsulated in a class in OOP unlike in C++ where it would stand alone, though C++ can also be used in OOP 
+// 
