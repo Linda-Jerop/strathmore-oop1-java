daa819175c316d49b65571f020f3ef918e89f86b
Linda <jeropkoimur@gmail.com>
2026-09-02 12:58:24 +0300

    class JFrame

---
+++ Week 3:/Lesson 2:/Demonstrations.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame; //import the class
+
+public class Demonstrations {
+    public static void main(String args[]){
+        //you may be asked to use some code to illustrate the difference between object creation and object declaration
+
+        JFrame helloWindow; //object declaration or variable declaration
+
+        helloWindow = new JFrame(); //object creation
+    }
+    
+}
+
